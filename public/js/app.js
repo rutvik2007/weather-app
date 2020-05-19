475c15a6014012fcb3df1c70ee689a0908b5147c
Rutvik <rutvik20007@gmail.com>
2020-05-19 17:04:36 -0400

    changed /public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log("Client side js is loaded")
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const outputBox = document.getElementById('forecast')
